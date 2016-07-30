91c8cd3cad371c89e1e00a8fef1209508e7ffbd9
Siva <sivabgs27@gmail.com>
2016-07-31 01:45:39 +0530

    Siva.. Added dependency body-parser

--- server_side/server.js
+++ server_side/server.js
@@ -1,14 +1,15 @@
 
 const express = require('express');
-
+var bodyParser = require('body-parser');
 const port = 8080;
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use('/', express.static('./client_side'));
 app.use('/', express.static('./'));
 
 app.post('/user', function (req, res) {
+  console.log( 'siva...' + JSON.stringify(req.body) );
   res.send('Got a POST request');
 });
 
